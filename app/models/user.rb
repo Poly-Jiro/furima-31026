3bc438a3a23789492ef840015befc1eb0c5afaf4
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-02 18:51:49 +0900

    ユーザー管理機能の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+   validates :family_name
+   validates :first_name
+   end
+
+   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+   validates :family_name_kana
+   validates :first_name_kana
+   end
+  
+   validates :nickname, presence: true
+   validates :birthday, presence: true
+
 end
