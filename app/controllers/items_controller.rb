6a47ebc4d6de485aa4710a528e96732675485ceb
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-05 00:00:46 +0900

    コンフリクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
 
   def destory
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :status_id, :fee_id, :source_id, :schedule_id, :category_id).merge(user_id: current_user.id)
+  end
 end
