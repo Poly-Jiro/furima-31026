f56c67233ac1a66873d049e683bac2ef0523d5ed
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-04 23:56:17 +0900

    Revert "コントローラーエラー修正"
    This reverts commit 31904ef865056b292e05cd165ac0a3d1344cc0b5.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,5 +16,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :text, :price, :status_id, :fee_id, :source_id, :schedule_id, :category_id).merge(user_id: current_user.id)
-  end
+
 end
