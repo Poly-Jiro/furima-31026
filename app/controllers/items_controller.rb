e475fb8580f23b894399690bc9ef68753e2c6ed4
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-04 15:56:26 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,8 @@ class ItemsController < ApplicationController
     
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :status_id, :fee_id, :source_id, :schedule_id, :category_id).merge(user_id: current_user.id)
+
 end
