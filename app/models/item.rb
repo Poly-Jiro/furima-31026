6c9fa5f1ce2f25ffb4a65b76ca78f27190236a66
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-04 16:11:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :text
+    validates :price
+    validates :user
+    validates :status_id
+    validates :fee_id
+    validates :source_id
+    validates :schedule_id 
+    validates :category_id
+  end
+
 end
