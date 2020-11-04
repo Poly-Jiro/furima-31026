c2a1210108ad948de54410050bda1cd81454fd19
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-04 23:57:11 +0900

    Revert "Active Storageの導入"
    This reverts commit e475fb8580f23b894399690bc9ef68753e2c6ed4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,8 +12,4 @@ class ItemsController < ApplicationController
     
   end
 
-  private
-  def item_params
-    params.require(:item).permit(:name, :text, :price, :status_id, :fee_id, :source_id, :schedule_id, :category_id).merge(user_id: current_user.id)
-
 end
