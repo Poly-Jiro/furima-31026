4d4978852dbf22f427ea97386481a4ca211ad57a
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-10 22:11:18 +0900

    destroyアクションの可読性、保守性向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :edit
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
   end
 
   def destroy
-    item = Item.find(params[:id])
-    redirect_to root_path if item.destroy
+    redirect_to root_path if @item.destroy
   end
 
   def new
@@ -53,3 +52,4 @@ class ItemsController < ApplicationController
     redirect_to action: :index unless user_signed_in? && (current_user.id == @item.user.id)
   end
 end
+destroy
