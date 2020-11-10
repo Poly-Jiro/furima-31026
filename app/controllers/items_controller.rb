35919cb9249a1fd8c21149cb183e704293564bfe
Poly-Jiro <never_disappear_this_ties@yahoo.co.jp>
2020-11-10 20:14:45 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,38 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :edit
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.order('created_at DESC')
-    # @item_name = Category.find_by(id: @items.)
   end
 
   def destory
-  end
-
-  def show
-    @item = Item.find(params[:id])
   end
 
   def new
     @item = Item.new
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to item_path(@item.id)
     else
       render :new
     end
@@ -29,4 +43,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :status_id, :fee_id, :source_id, :schedule_id, :category_id).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && (current_user.id == @item.user.id) 
+      redirect_to action: :index
+    end
+  end
+
 end
